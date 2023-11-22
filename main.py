7c44f0737954f04d9f72a34d828754230ec06469
kenniele <kenniele@yandex.com>
2023-11-22 19:44:40 +0300

    gluhjv proveril

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pickle
 import functools
 from copy import deepcopy
 class Table:
-    @functools.singledispatchmethod
-    def __init__(self, data=[]):
-        raise TypeError("Введенные данные не являются строкой")
-    @__init__.register(str)
     def __init__(self, data=[]):
         self.filename = ""
         self.data = []
@@ -27,33 +23,36 @@ class Table:
                                 self.data.update({pair[0]: pair[1]})
                 elif self.filename.endswith(".csv"):
                     with open(self.filename, encoding="utf-8") as f:
-                        temp = csv.DictReader(f, quotechar='"')
+                        temp = csv.DictReader(f, delimiter=";", quotechar='"')
+                        for x in temp:
+                            self.data.append(x)
                 elif self.filename.endswith(".pkl"):
                     with open(self.filename, 'rb') as f:
                         self.data = pickle.load(f)
                         self.header = list(self.data.keys())
                 else:
                     raise AttributeError("Недоступное расширение!")
+            self.header = list(self.data[0].keys())
             self.column_types = self.get_column_types(by_number=False)
         except:
             raise KeyError("Несовместимые имена таблицы!")
-        def save_table(self, new_filename):
-            if new_filename.endswith(".txt"):
-                with open(new_filename, 'w', encoding='utf-8') as f:
-                    f.write('\t'.join(self.data[0].keys()))
-                    for row in self.data:
-                        f.write('\t'.join(row.values()))
-            elif new_filename.endswith(".csv"):
-                with open(new_filename, 'w', encoding='utf-8') as f:
-                    writer = csv.writer(f)
-                    writer.writerow(self.header)
-                    for row in self.data:
-                        writer.writerow(row.values())
-            elif new_filename.endswith(".pkl"):
-                with open(new_filename, "wb") as f:
-                    pickle.dump(self.data, f)
-            else:
-                raise AttributeError("Недоступное расширение!")
+    def save_table(self, new_filename):
+        if new_filename.endswith(".txt"):
+            with open(new_filename, 'w', encoding='utf-8') as f:
+                f.write('\t'.join(self.data[0].keys()))
+                for row in self.data:
+                    f.write('\t'.join(row.values()))
+        elif new_filename.endswith(".csv"):
+            with open(new_filename, 'w', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow(self.header)
+                for row in self.data:
+                    writer.writerow(row.values())
+        elif new_filename.endswith(".pkl"):
+            with open(new_filename, "wb") as f:
+                pickle.dump(self.data, f)
+        else:
+            raise AttributeError("Недоступное расширение!")
     @staticmethod
     def concat(table1, table2):
         if isinstance(table1, Table) and isinstance(table2, Table):
@@ -72,8 +71,8 @@ class Table:
     def get_rows_by_number(self, start, stop=None, copy_table=False):
         try:
             new_d = self.data.copy() if copy_table else deepcopy(self.data)
-            stop = stop if stop else len(new_d)
-            return [new_d[i] for i in range(start, stop)]
+            stop = stop if stop else len(new_d) - 1
+            return [new_d[i] for i in range(start, stop + 1)]
         except:
             raise ValueError("Произошла непредвиденная ошибка при получении данных из файла")
     def get_rows_by_index(self, *args, copy_table=False):
@@ -110,23 +109,23 @@ class Table:
                 exec(f"lst[self.header[i]] = {types_dict[[self.header[i], i][by_number]]}(lst[self.header[i]])")
     def get_values(self, column=0):
         if isinstance(column, int):
-            column = list(self.data.keys())[column]
+            column = list(self.data[0].keys())[column]
         return [self.data[i][column] for i in range(len(self.data))]
     def get_value(self, column=0):
         if isinstance(column, int):
-            column = list(self.data.keys())[column]
+            column = list(self.data[0].keys())[column]
         return self.data[0][column]
     def set_values(self, values, column=0):
         if isinstance(column, int):
-            column = list(self.data.keys())[column]
+            column = list(self.data[0].keys())[column]
         for i in range(len(self.data)):
-            self.data[i][column] = values[i % len(self.data)]
+            self.data[i][column] = values[i % len(values)]
     def set_value(self, value, column=0):
         if isinstance(column, int):
-            column = list(self.data.keys())[column]
+            column = list(self.data[0].keys())[column]
         self.data[0][column] = value
     def print_table(self):
-        print(*list(self.data.keys()), sep=" ")
+        print(*list(self.header), sep=" ")
         for i in range(len(self.data)):
             print(*list(self.data[i].values()), sep=" ")
     @staticmethod
